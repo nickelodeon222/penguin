88385dae005e60167c8f46766776c11f74114d02
nickelodeon222 <sandiwch34@gmail.com>
2023-02-14 20:20:29 +1300

    "working" "prototype"

---
+++ Main.cs
@@ -0,0 +1,28 @@
+using Godot;
+using System;
+
+public class Main : Node2D
+{
+    // Declare member variables here. Examples:
+    // private int a = 2;
+    // private string b = "text";
+    Player player;
+
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        player = GetNode<Player>("Player");
+    }
+
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(float delta)
+    {
+        player.Position = new Vector2(131, player.Position.y);
+
+        GetNode<Sprite>("Sprite").Position -= Vector2.Right * player.velocity * delta;
+
+        player.Speed += delta * 2;
+
+        GD.Print(player.Speed);
+    }
+}
--- Player.cs
+++ Player.cs
@@ -3,9 +3,9 @@ using System;
 
 public class Player : KinematicBody2D
 {
-    // Declare member variables here. Examples:
-    // private int a = 2;
-    // private string b = "text";
+    [Export]
+    public float Speed;
+    public Vector2 velocity;
 
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
@@ -13,9 +13,24 @@ public class Player : KinematicBody2D
         
     }
 
-//  // Called every frame. 'delta' is the elapsed time since the previous frame.
-//  public override void _Process(float delta)
-//  {
-//      
-//  }
+    private float mouseMove = 0;
+
+    public override void _Input(InputEvent @event)
+    {
+        if (@event is InputEventMouseMotion mouseEvent) {
+            mouseMove += Mathf.Atan(mouseEvent.Relative.y * 0.05f) * 0.5f * Mathf.Cos(Rotation * 0.2f);
+        }
+    }
+
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _PhysicsProcess(float delta)
+    {
+        Rotate(mouseMove);
+        mouseMove = 0;
+
+        Rotation = Mathf.Clamp(Rotation, -Mathf.Pi/2, Mathf.Pi/2);
+        velocity = (Transform.x + Vector2.Right / 2) * Speed;
+
+        MoveAndSlide(velocity, Vector2.Up);
+    }
 }
